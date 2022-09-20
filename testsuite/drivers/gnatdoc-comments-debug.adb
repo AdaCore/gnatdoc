3c5a389fc7b9e43294b208451ef906ba435d4867
Vadim Godunko <godunko@adacore.com>
2022-09-20 17:49:40 +0300

    V831-009 Add missing files.

---
+++ testsuite/drivers/gnatdoc-comments-debug.adb
@@ -0,0 +1,52 @@
+------------------------------------------------------------------------------
+--                    GNAT Documentation Generation Tool                    --
+--                                                                          --
+--                       Copyright (C) 2022, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Wide_Wide_Text_IO;   use Ada.Wide_Wide_Text_IO;
+
+with VSS.Strings.Conversions; use VSS.Strings.Conversions;
+
+with Langkit_Support.Slocs;   use Langkit_Support.Slocs;
+
+package body GNATdoc.Comments.Debug is
+
+   ----------
+   -- Dump --
+   ----------
+
+   procedure Dump (Comment : Structured_Comment'Class) is
+   begin
+      for Section of Comment.Sections loop
+         Put_Line
+           ("\/ "
+            & Section_Kind'Wide_Wide_Image (Section.Kind)
+            & " "
+            & To_Wide_Wide_String (Section.Symbol)
+            & " ("
+            & To_Wide_Wide_String (Section.Name)
+            & ") "
+            & Line_Number'Wide_Wide_Image (Section.Exact_Start_Line)
+            & Line_Number'Wide_Wide_Image (Section.Exact_End_Line)
+            & Line_Number'Wide_Wide_Image (Section.Group_Start_Line)
+            & Line_Number'Wide_Wide_Image (Section.Group_End_Line));
+
+         for Line of Section.Text loop
+            Put_Line (To_Wide_Wide_String (Line));
+         end loop;
+      end loop;
+   end Dump;
+
+end GNATdoc.Comments.Debug;
