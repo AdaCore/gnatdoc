ad606d582fdd92d4e780d972e3700a40bfc8e665
Vadim Godunko <godunko@adacore.com>
2022-11-24 19:23:26 +0300

    Document code.

--- source/gnatdoc-comments-builders.ads
+++ source/gnatdoc-comments-builders.ads
@@ -15,6 +15,9 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
+--  This packages provides base tagged type to construct set of sections of
+--  structured comment.
+
 private with Langkit_Support.Slocs;
 private with Libadalang.Analysis;
 
@@ -63,14 +66,23 @@ private
    procedure Process_Component_Declaration
      (Self : in out Abstract_Components_Builder'Class;
       Node : Libadalang.Analysis.Ada_Node'Class);
+   --  Prepare to construct set of sections for declaration. Declaration can
+   --  define few defining names, Process_Defining_Name subprogram must be
+   --  called to create actual section for each such name.
 
    procedure Process_Defining_Name
      (Self : in out Abstract_Components_Builder'Class;
       Kind : GNATdoc.Comments.Section_Kind;
       Node : Libadalang.Analysis.Defining_Name'Class);
+   --  Create section for the given defining name.
 
    procedure Restart_Component_Group
      (Self       : in out Abstract_Components_Builder'Class;
       Start_Line : Langkit_Support.Slocs.Line_Number);
+   --  When processing of the component groups (group of declarations that
+   --  shares documentation) is enabled, call of this subprogram reset
+   --  tracking of the group. It must be used then some syntactic elements
+   --  breaks group, for instance, t shpuld be called after processing of the
+   --  discriminants of the record type, and before processing of the fields.
 
 end GNATdoc.Comments.Builders;
