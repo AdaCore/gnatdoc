635dbe8a0c7c601325bf33fabc31a1084e14a8ef
Vadim Godunko <godunko@adacore.com>
2024-10-01 18:08:01 +0400

    Automated test for renamings.

---
+++ testsuite/extractor/renamings/pkg.ads
@@ -0,0 +1,20 @@
+
+--  This is test data for test of documentation extraction for renamings.
+
+package Pkg is
+
+   E : exception;
+
+   K : Integer;
+
+   procedure P;
+
+   function F return Integer;
+
+   generic package GPkg;
+
+   generic procedure GP;
+
+   generic function GF return Integer;
+
+end Pkg;
