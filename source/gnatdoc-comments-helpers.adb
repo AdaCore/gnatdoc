47eaaba08c6ef3fa74f8c4e9d91afed6fa0a418f
Vadim Godunko <godunko@adacore.com>
2025-10-20 09:17:57 +0400

    Fix handling of generic package instantiations.

--- source/gnatdoc-comments-helpers.adb
+++ source/gnatdoc-comments-helpers.adb
@@ -488,6 +488,7 @@ package body GNATdoc.Comments.Helpers is
                | Ada_Enum_Literal_Decl
                | Ada_Exception_Decl
                | Ada_For_Loop_Var_Decl
+               | Ada_Generic_Package_Instantiation
                | Ada_Generic_Package_Decl
                | Ada_Generic_Subp_Decl
                | Ada_Incomplete_Type_Decl
