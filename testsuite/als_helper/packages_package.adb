978fb84aca3a9feb202a7b51db24cc4e19353f59
Vadim Godunko <godunko@adacore.com>
2025-11-12 12:52:22 +0400

    Testcase of package.
    Incomplete due to LAL's bug.

---
+++ testsuite/als_helper/packages_package.adb
@@ -0,0 +1,11 @@
+with Packages;
+with Packages_Renaming;
+
+package body Packages_Package is
+
+   procedure Dummy is
+   begin
+      Packages.Dummy;
+   end Dummy;
+
+end Packages_Package;
--- testsuite/als_helper/pkg2.adb
+++
@@ -1,9 +0,0 @@
-
-package body Pkg2 is
-
-   procedure Dummy is
-   begin
-      Pkg1.Dummy;
-   end Dummy;
-
-end Pkg2;
