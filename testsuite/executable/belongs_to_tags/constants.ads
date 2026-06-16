b0f9cb91c029b3b5b41e2ee2bc729c3b22710592
Vadim Godunko <godunko@adacore.com>
2026-06-16 18:39:59 +0400

    Update test

--- testsuite/executable/belongs_to_tags/constants.ads
+++ testsuite/executable/belongs_to_tags/constants.ads
@@ -15,4 +15,11 @@ package Constants is
    BI : constant TI := 10;
    --  @belongs-to TI
 
+   type TR is null record;
+
+   CR : constant TR := (null record);
+
+   BR : constant TR := (null record);
+   --  @belongs-to TR
+
 end Constants;
